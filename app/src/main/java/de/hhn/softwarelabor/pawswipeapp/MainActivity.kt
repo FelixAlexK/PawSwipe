3763db78a7c0342cb1dcd075074eeb384c5613d6
nwendler <nwendler@stud.hs-heilbronn.de>
2023-05-17 13:19:42 +0200

    Added Splash Screen displaying PawSwipe animation

--- app/src/main/java/de/hhn/softwarelabor/pawswipeapp/MainActivity.kt
+++ app/src/main/java/de/hhn/softwarelabor/pawswipeapp/MainActivity.kt
@@ -2,19 +2,24 @@ package de.hhn.softwarelabor.pawswipeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    /** Duration of wait **/
+    private val SPLASH_DISPLAY_LENGTH = 3400
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        // Inside the onClick method of a button or any other event listener
-        val intent = Intent(this, RegisterAccountActivity::class.java)
-
-        startActivity(intent)
-
+        //Splash screen
+        Handler().postDelayed({
+            val intent = Intent(this, RegisterAccountActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
     }
 }
