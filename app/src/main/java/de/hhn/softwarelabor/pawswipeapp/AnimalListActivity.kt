30b4d077e7e71670f0cc8973a76380def88ef81d
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-06-17 20:29:18 +0200

    added functions to list liked animals

--- app/src/main/java/de/hhn/softwarelabor/pawswipeapp/AnimalListActivity.kt
+++ app/src/main/java/de/hhn/softwarelabor/pawswipeapp/AnimalListActivity.kt
@@ -5,23 +5,63 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import de.hhn.softwarelabor.pawswipeapp.api.animal.AnimalProfileApi
+import de.hhn.softwarelabor.pawswipeapp.api.like.LikeApi
+import de.hhn.softwarelabor.pawswipeapp.utils.AnimalAdapter
+import de.hhn.softwarelabor.pawswipeapp.utils.AnimalItem
 
+
+/**
+ * AnimalListActivity is an AppCompatActivity that displays a list of liked animals.
+ * @author Felix Kuhbier & Leo Kalmbach
+ *
+ * @property matchBtn Button to navigate to the MatchActivity.
+ * @property chatBtn Button to navigate to the ChatActivity.
+ * @property recyclerView RecyclerView that displays the list of liked animals.
+ * @property animalAdapter AnimalAdapter for managing the list of liked animals in the RecyclerView.
+ * @property animalItems ArrayList of AnimalItem objects representing the liked animals.
+ * @property profileId Integer representing the profile ID of the current user.
+ */
 class AnimalListActivity : AppCompatActivity() {
 
     private lateinit var matchBtn: Button
     private lateinit var chatBtn: Button
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var animalAdapter: AnimalAdapter
+    private var animalItems: ArrayList<AnimalItem> = ArrayList()
+    private var profileId: Int = 0
+
+    /**
+     * Initializes the activity, sets up the UI and loads the liked animals.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animal_list)
 
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
 
+        profileId = intent.getIntExtra("id", 0)
+
+        getAnimalItemsFromAnimalIds(profileId)
+        animalAdapter = AnimalAdapter(animalItems)
+
+
+        recyclerView = findViewById(R.id.animal_list_recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = animalAdapter
+
         matchBtn = findViewById(R.id.matching_btn3)
         matchBtn.setOnClickListener {
             val intent = Intent(this@AnimalListActivity, MatchActivity::class.java)
+            intent.putExtra("id", profileId)
             startActivity(intent)
         }
 
@@ -30,14 +70,22 @@ class AnimalListActivity : AppCompatActivity() {
             val intent = Intent(this@AnimalListActivity, ChatActivity::class.java)
             startActivity(intent)
         }
+
+        updateEmptyTextViewVisibility()
     }
 
+    /**
+     * Inflates the options menu.
+     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu_home,menu)
+        inflater.inflate(R.menu.menu_home, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    /**
+     * Handles the selected option from the options menu.
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_settings -> {
@@ -45,26 +93,31 @@ class AnimalListActivity : AppCompatActivity() {
                 startActivity(intent)
                 true
             }
+
             R.id.menu_animalServices -> {
                 val intent = Intent(this@AnimalListActivity, AnimalServiceActivity::class.java)
                 startActivity(intent)
                 true
             }
+
             R.id.menu_animalEdit -> {
                 val intent = Intent(this@AnimalListActivity, EditAnimalActivity::class.java)
                 startActivity(intent)
                 true
             }
+
             R.id.menu_animalCreate -> {
                 val intent = Intent(this@AnimalListActivity, PetProfileActivity::class.java)
                 startActivity(intent)
                 true
             }
+
             R.id.menu_logOut -> {
                 val intent = Intent(this@AnimalListActivity, LoginActivity::class.java)
                 startActivity(intent)
                 true
             }
+
             R.id.menu_filter -> {
                 val intent = Intent(this@AnimalListActivity, FilterActivity::class.java)
                 startActivity(intent)
@@ -74,4 +127,69 @@ class AnimalListActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    /**
+     * Updates the visibility of the emptyTextView based on the size of the animalItems ArrayList.
+     */
+    private fun updateEmptyTextViewVisibility() {
+        val emptyTextView: TextView = findViewById(R.id.animal_list_empty_textView)
+        emptyTextView.visibility = if (animalItems.isEmpty()) View.VISIBLE else View.GONE
+    }
+
+    /**
+     * Retrieves AnimalItem objects based on the liked animal IDs for the given profileId.
+     *
+     * @param profileId Integer representing the profile ID of the current user.
+     */
+    private fun getAnimalItemsFromAnimalIds(profileId: Int) {
+        val likeApi = LikeApi()
+        val animalProfileApi = AnimalProfileApi()
+
+        //get all the IDs of animals you like
+        likeApi.getLikedAnimals(profileId) { response, error ->
+            if (error != null) {
+                runOnUiThread {
+                    Toast.makeText(
+                        this@AnimalListActivity,
+                        "Tiere konnten nicht geladen werden",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else {
+                response?.forEach { id ->
+
+                    //Get profiles from IDs and add them to the list
+                    animalProfileApi.getAnimalProfileByID(id) { response, error ->
+
+                        runOnUiThread {
+                            if (error != null) {
+
+                                Toast.makeText(
+                                    this@AnimalListActivity,
+                                    "${error.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                            } else {
+
+
+                                val item = AnimalItem(
+                                    R.drawable.pixabay_cute_cat, response?.name,
+                                    response?.species, response?.breed
+                                )
+                                animalAdapter.addItem(item)
+                                updateEmptyTextViewVisibility()
+
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+    }
 }
+
+
+
