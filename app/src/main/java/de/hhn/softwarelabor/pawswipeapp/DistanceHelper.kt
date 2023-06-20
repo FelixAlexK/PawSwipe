094ebee7dc2b0a1a6fe59da2f1e654b1f97e22e3
lkalmbach <lkalmbach@stud.hs-heilbronn.de>
2023-06-20 10:10:11 +0200

    added method for calculating distance between two locations

--- app/src/main/java/de/hhn/softwarelabor/pawswipeapp/DistanceHelper.kt
+++ app/src/main/java/de/hhn/softwarelabor/pawswipeapp/DistanceHelper.kt
@@ -1,5 +1,8 @@
 package de.hhn.softwarelabor.pawswipeapp
 
+import java.lang.Math.*
+import java.text.DecimalFormat
+
 class DistanceHelper {
     private fun getShelterInRange(
         currentLat: Double,
@@ -9,11 +12,11 @@ class DistanceHelper {
         lon: Double
     ): Boolean {
         val earthRadius = 6371.0 // Radius der Erde in Kilometern
-        val dLat = Math.toRadians(lat - currentLat)
-        val dLon = Math.toRadians(lon - currentLong)
+        val dLat = toRadians(lat - currentLat)
+        val dLon = toRadians(lon - currentLong)
         val a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(currentLat)) * Math.cos(
-                Math.toRadians(lat)
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(toRadians(currentLat)) * Math.cos(
+                toRadians(lat)
             ) *
                     Math.sin(dLon / 2) * Math.sin(dLon / 2)
         val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
@@ -21,5 +24,19 @@ class DistanceHelper {
 
         // Überprüfung, ob die Distanz innerhalb des angegebenen Radius liegt
         return distanceInKm <= distance
+    }
+
+    private fun calculateDistance(lat1: Double, long1: Double, lat2: Double, long2: Double): String {
+        val earthRadius = 6371 // Durchmesser der Erde in Kilometern
+        val decimalFormat = DecimalFormat("#.###")
+
+        val distanceLat = toRadians(lat2 - lat1)
+        val distanceLong = toRadians(long2 - long1)
+
+        val a = sin(distanceLat / 2) * sin(distanceLat / 2) + cos(toRadians(lat1)) *
+                cos(toRadians(lat2)) * sin(distanceLong / 2) * sin(distanceLong / 2)
+        val b = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        return decimalFormat.format(earthRadius * b)
     }
 }
