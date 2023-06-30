bb525d85088faf6f5eb761e71c25b164546644c5
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-07-01 01:57:10 +0200

    created Filter api

---
+++ app/src/main/java/de/hhn/softwarelabor/pawswipeapp/api/filter/FilterEnum.kt
@@ -0,0 +1,15 @@
+package de.hhn.softwarelabor.pawswipeapp.api.filter
+
+enum class FilterEnum(val filter: String) {
+
+    SPECIES("species"),
+    COLOR("color"),
+    AGE_MIN("age_min"),
+    AGE_MAX("age_max"),
+    ILLNESS("illness"),
+    BREED("breed"),
+    GENDER("gender"),
+    PROFILE_ID("profile_id"),
+
+
+}
