0787c80ce53a44fb3c9e5f9655ac45e126c7184b
aln0c <127910471+aln0c@users.noreply.github.com>
2023-07-03 16:16:33 +0200

    Popup when clicking

--- app/src/main/java/de/hhn/softwarelabor/pawswipeapp/MatchActivity.kt
+++ app/src/main/java/de/hhn/softwarelabor/pawswipeapp/MatchActivity.kt
@@ -1,6 +1,8 @@
 package de.hhn.softwarelabor.pawswipeapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
@@ -14,8 +16,13 @@ import android.view.View
 import android.view.animation.AccelerateInterpolator
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -27,6 +34,7 @@ import de.hhn.softwarelabor.pawswipeapp.api.animal.AnimalProfileApi
 import de.hhn.softwarelabor.pawswipeapp.api.data.AnimalProfileData
 import de.hhn.softwarelabor.pawswipeapp.api.like.LikeApi
 import de.hhn.softwarelabor.pawswipeapp.utils.AppData
+import de.hhn.softwarelabor.pawswipeapp.utils.Base64Utils
 import kotlin.math.abs
 
 
@@ -58,10 +66,29 @@ class MatchActivity : AppCompatActivity(), CardStackListener {
     private lateinit var adapter: CardAdapter
     private lateinit var cardStackView: CardStackView
     private lateinit var layoutManager: CardStackLayoutManager
-    private var currentPosition = 1
+    private lateinit var linLayoutCard: LinearLayout
+    private var currentPosition = 0
     var isDragging = false
     var startX = 0f
     var startY = 0f
+    
+    
+    private lateinit var detailedAnimalName: TextView
+    private lateinit var detailedAnimalPicture: ImageView
+    private lateinit var detailedAnimalBreed: TextView
+    private lateinit var detailedAnimalSpecies: TextView
+    private lateinit var detailedAnimalBirthday: TextView
+    private lateinit var detailedAnimalPreExistingIllness: TextView
+    private lateinit var detailedAnimalColor: TextView
+    private lateinit var detailedAnimalGender: TextView
+    private lateinit var detailedShelterPhone: TextView
+    private lateinit var detailedShelterEmail: TextView
+    private lateinit var detailedShelterPhoneHint: TextView
+    private lateinit var detailedShelterEmailHint: TextView
+    private lateinit var detailedAnimalDislikeButton: ImageButton
+    
+    
+    private lateinit var currentAnimal: AnimalProfileData
     
     
     @Deprecated("Deprecated in Java")
@@ -88,17 +115,23 @@ class MatchActivity : AppCompatActivity(), CardStackListener {
         profileId = AppData.getID(this@MatchActivity)
     }
     
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match)
         
         cardStackView = findViewById(R.id.matchCardStackView)
+        linLayoutCard = findViewById(R.id.linearlayoutCardView)
         
         
         layoutManager = CardStackLayoutManager(this@MatchActivity, this@MatchActivity)
         layoutManager.setStackFrom(StackFrom.None)
         layoutManager.setVisibleCount(1)
         cardStackView.layoutManager = layoutManager
+        
+        cardStackView.layoutParams.width = resources.displayMetrics.widthPixels
+        cardStackView.layoutParams.height = resources.displayMetrics.widthPixels
+        
         
         getAllAnimals()
         
@@ -153,42 +186,131 @@ class MatchActivity : AppCompatActivity(), CardStackListener {
         }
         
         cardStackView.setOnTouchListener { _, motionEvent ->
-            when (motionEvent.action){
+            when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
                     isDragging = false
                     startX = motionEvent.x
                     startY = motionEvent.y
                     true
                 }
+                
                 MotionEvent.ACTION_MOVE -> {
-                    if (abs(motionEvent.x - startX) > 10 || abs(motionEvent.y) > 10){
+                    if (abs(motionEvent.x - startX) > 10 || abs(motionEvent.y) > 10) {
                         isDragging = true
                     }
                     false
                 }
+                
                 MotionEvent.ACTION_UP -> {
                     if (!isDragging) {
-                        Toast.makeText(
-                            this@MatchActivity,
-                            "Clicked",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        //START
+    
+                        currentAnimal = adapter.getAnimal(currentPosition)
+    
+                        val customLayout =
+                            layoutInflater.inflate(R.layout.activity_detailed_animal_profile, null)
+    
+    
+                        detailedAnimalName = customLayout.findViewById(R.id.detailedAnimalName_textView)
+                        detailedAnimalPicture =
+                            customLayout.findViewById(R.id.detailedAnimalPicture_imageView)
+                        detailedAnimalBreed =
+                            customLayout.findViewById(R.id.detailedAnimalBreed_textView)
+                        detailedAnimalBirthday =
+                            customLayout.findViewById(R.id.detailedAnimalBirthday_textView)
+                        detailedAnimalSpecies =
+                            customLayout.findViewById(R.id.detailedAnimalSpecies_textView)
+                        detailedAnimalPreExistingIllness =
+                            customLayout.findViewById(R.id.detailedAnimalPreExistingIllness_textView)
+                        detailedAnimalColor =
+                            customLayout.findViewById(R.id.detailedAnimalColor_textView)
+                        detailedAnimalGender =
+                            customLayout.findViewById(R.id.detailedAnimalGender_textView)
+                        detailedShelterPhone =
+                            customLayout.findViewById(R.id.detailedAnimalPhone_textView)
+                        detailedShelterEmail =
+                            customLayout.findViewById(R.id.detailedAnimalEmail_textView)
+                        detailedShelterPhoneHint =
+                            customLayout.findViewById(R.id.detailedAnimalPhoneHint_textView)
+                        detailedShelterEmailHint =
+                            customLayout.findViewById(R.id.detailedAnimalEmailHint_textView)
+                        detailedAnimalDislikeButton = customLayout.findViewById(R.id.detailedAnimalDislike_imageButton)
+    
+    
+    
+    
+                        detailedAnimalName.text = currentAnimal.name
+                        currentAnimal.picture_one?.let { pictureOne ->
+                            // Dekodiere das Bild aus dem Base64-String
+                            val decodedBitmap = Base64Utils.decode(pictureOne)
+        
+                            val width = decodedBitmap.width
+                            val height = decodedBitmap.height
+        
+                            val targetSize = height.coerceAtMost(width)
+        
+                            val x = (width - targetSize) / 2
+                            val y = (height - targetSize) / 2
+        
+                            // Skaliere das Bild entsprechend der gewünschten Breite und Höhe
+                            val scaledBitmap =
+                                Bitmap.createBitmap(decodedBitmap, x, y, targetSize, targetSize)
+                            // Oder:
+                            // val scaledBitmap = BitmapScaler.scaleToFitHeight(decodedBitmap, screenHeight)
+        
+                            // Setze das skalierte Bild in das ImageView
+                            detailedAnimalPicture.setImageBitmap(scaledBitmap)
+                        }
+                        detailedAnimalSpecies.text = currentAnimal.species
+                        detailedAnimalBreed.text = currentAnimal.breed
+                        detailedAnimalGender.text = currentAnimal.gender
+                        detailedAnimalColor.text = currentAnimal.color
+                        detailedAnimalPreExistingIllness.text = currentAnimal.illness
+    
+                        detailedAnimalBirthday.text = currentAnimal.birthday
+    
+                        detailedAnimalDislikeButton.visibility = View.GONE
+    
+                        detailedShelterEmail.text = resources.getString(R.string.like_to_see)
+                        detailedShelterPhone.text = resources.getString(R.string.like_to_see)
+    
+    
+                        val builder = AlertDialog.Builder(this)
+                        builder.setView(customLayout)
+    
+                        val dialog: AlertDialog = builder.create()
+    
+    
+                        dialog.show()
+    
+                        val outerLayout: ConstraintLayout? = dialog.findViewById(R.id.outer_layout)
+                        outerLayout?.setOnClickListener {
+                            dialog.dismiss()
+                        }
+    
+                        //ENDE
                     }
+
                     isDragging = false
                     false
                 }
+                
                 else -> false
             }
             
         }
-
+        
         imageList = imageList + R.drawable.pixabay_cute_cat
         imageList = imageList + R.drawable.dislike
         imageList = imageList + R.drawable.love
         imageList = imageList + R.drawable.wf
         imageList = imageList + R.drawable.paw_swipe_splash_screen
         
-        
+        Toast.makeText(
+            this@MatchActivity,
+            currentPosition.toString(),
+            Toast.LENGTH_SHORT
+        ).show()
     }
     
     /**
@@ -285,9 +407,11 @@ class MatchActivity : AppCompatActivity(), CardStackListener {
         likeApi.dislikeAnimal(profileId, animalId) { response, error ->
             runOnUiThread {
                 if (error != null) {
-                    if (error.message != "Error: 500"){
+                    if (error.message != "Error: 500") {
                         Toast.makeText(
-                            this@MatchActivity, getString(R.string.like_error_text), Toast.LENGTH_SHORT
+                            this@MatchActivity,
+                            getString(R.string.like_error_text),
+                            Toast.LENGTH_SHORT
                         ).show()
                     }
                 } else if (response?.isSuccessful == true) {
@@ -312,9 +436,12 @@ class MatchActivity : AppCompatActivity(), CardStackListener {
             when (direction) {
                 Direction.Left -> {
                     dislikeAnimal(profileId, animal.animal_id!!)
+                    currentPosition++
                 }
+                
                 Direction.Right -> {
                     likeAnimal(profileId, animal.animal_id!!)
+                    currentPosition++
                 }
                 
                 else -> {
@@ -326,6 +453,7 @@ class MatchActivity : AppCompatActivity(), CardStackListener {
     }
     
     override fun onCardRewound() {
+        currentPosition--
     }
     
     override fun onCardCanceled() {
@@ -335,7 +463,6 @@ class MatchActivity : AppCompatActivity(), CardStackListener {
     }
     
     override fun onCardDisappeared(view: View?, position: Int) {
-        currentPosition = position
     }
     
     private fun getAllAnimals() {
